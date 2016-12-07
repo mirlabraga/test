eab4ae56c39520198d113dbb84c7d320ce494b09
Mirla Braga <mirlabraga@gmail.com>
2016-12-07 10:02:47 -0200

    Refactore model

--- fiveware-test-model/src/main/java/br/com/test/repositorio/UsuarioRepository.java
+++ fiveware-test-model/src/main/java/br/com/test/repositorio/UsuarioRepository.java
@@ -1,23 +1,10 @@
 package br.com.test.repositorio;
 
-import java.util.Collection;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 
 import br.com.test.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
-
-	@Query("SELECT DISTINCT usuario FROM Usuario usuario WHERE usuario.nomeCurto LIKE :nomeCurto%")
-	@Transactional(readOnly = true)
-	Collection<Usuario> procurarPorNomeCurto(
-			@Param("nomeCurto") String nomeCurto);
-
-	@Query("SELECT usuario FROM Usuario usuario WHERE usuario.id =:id")
-	@Transactional(readOnly = true)
-	Usuario procurarPorId(@Param("id") Integer id);
-
 }
